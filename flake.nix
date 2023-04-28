8c42b42428b329161a1eceeec8d37c0ef52cf54c
Noah D. Ortiz <abstrnoah@brumal.net>
2023-04-27 17:11:39 -0700

    flake apps jekyll, serve

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
   outputs =
-    inputs@{ ... }:
+    inputs@{ self, ... }:
     let
       system = "x86_64-linux";
       nixpkgs = inputs.nixpkgs.legacyPackages.${system};
@@ -17,6 +17,27 @@
           gemfile = ./Gemfile;
           lockfile = ./Gemfile.lock;
           gemset = ./gemset.nix;
+        };
+      };
+
+      apps.${system} = rec {
+        default = serve;
+
+        jekyll = {
+          type = "app";
+          program = "${self.packages.${system}.bundler_env}/bin/jekyll";
+        };
+
+        serve = {
+          type = "app";
+          program =
+            let
+              app = nixpkgs.writeShellApplication {
+                name = "jekyll-serve";
+                text = ''${jekyll.program} serve "$@"'';
+              };
+            in
+            "${app}/bin/${app.name}";
         };
       };
     };
